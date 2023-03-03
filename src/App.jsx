ea5b6a4c7ebd155136a9897e1cc924ad247b9547
Joshua Atanu <atanu.joshua@gmail.com>
2023-03-03 17:10:27 +0100

    completed project

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,44 @@
 import { useState } from "react";
 import { Configuration, OpenAIApi } from "openai";
 import "./App.css";
+import OptionSelection from "./components/OptionSelection";
+import Translation from "./components/Translation";
+import { arrayItems } from "./AIOptions";
 
 function App() {
-  const [prompt, setPrompt] = useState("");
-  const [result, setResult] = useState("");
-
- 
-
   const configuration = new Configuration({
-    apiKey:import.meta.env.VITE_OPENAI_API_KEY,
+    apiKey: import.meta.env.VITE_OPENAI_API_KEY,
   });
+
   const openai = new OpenAIApi(configuration);
-  const generateResponse = async () => {
-    const res = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: prompt,
-      temperature: 0.7,
-      max_tokens: 1084,
-      top_p: 1,
-      best_of: 8,
-      frequency_penalty: 0,
-      presence_penalty: 0,
-    });
-    setResult(res.data.choices[0].text);
+  const [option, setOption] = useState({});
+  const [result,setResult]= useState("");
+  const [input, setInput] = useState("");
+
+  const selectOption = (option) => {
+    setOption(option);
   };
+
+  const doStuff = async () => {
+    let object = { ...option, prompt: input };
+    // console.log(object);
+    const response = await openai.createCompletion(object);
+    setResult(response.data.choices[0].text)
+  };
+  console.log(option);
+
+  // const generateResponse = async () => {
+  //   const res = await openai.createCompletion({});
+  //   setResult(res.data.choices[0].text);
+  // };
   return (
-    <div className="app-main">
-      <h3>chatGPT</h3>
-      <input
-        placeholder="type something"
-        onChange={(e) => setPrompt(e.target.value)}
-      ></input>
-      <button onClick={generateResponse}> submit</button>
-      <h4> {result}</h4>
-    </div>
+    <>
+      {Object.values(option).length === 0 ? (
+        <OptionSelection arrayItems={arrayItems} selectOption={selectOption} />
+      ) : (
+        <Translation doStuff={doStuff} setInput={setInput} result={result}/>
+      )}
+    </>
   );
 }
 
